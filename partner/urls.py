c66b2e28418d6f2dbbd9b2eca1751eccbde6ae33
army04 <yrlee0412@gmail.com>
2019-06-03 17:02:48 +0900

    Change directory

---
+++ partner/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url, include
+from .views import (
+    index,
+    signup, login,logout,
+)
+
+urlpatterns = [
+    url(r'^$',index, name="index"),
+    url(r'^signup/$', signup, name="signup"),
+    url(r'^login/$', login,name="login"),
+    url(r'^logout/$', logout,name="logout"),
+]
--- partner/views.py
+++ partner/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from django.contrib.auth import (
+    authenticate,
+    login as auth_login,
+    logout as auth_logout,
+)
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
+def index(request):
+    ctx = {}
+    return render(request, "index.html",ctx)
+
+def login(request):
+    ctx = {}
+
+    if request.method == "GET":
+        pass
+    elif request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            auth_login(request, user)
+            return redirect("/partner/")
+        else:
+            ctx.update({"error":"사용자가 없습니다."})
+
+    return render(request, "login.html",ctx)
+
+def signup(request):
+    if request.method == "GET":
+        pass
+    elif request.method == "POST":
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+
+        user = User.objects.create_user(username, email, password)
+
+    ctx={}
+    return render(request, "signup.html",ctx)
+
+def logout(request):
+    auth_logout(request)
+    return redirect("/partner/")    
